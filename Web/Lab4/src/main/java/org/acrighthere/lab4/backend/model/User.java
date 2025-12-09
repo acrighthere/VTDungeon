67252a4ba3cd9bde073a4fb90efd219ef839e52d
acrighthere <rodionovmaksprog@mail.ru>
2025-12-10 01:21:41 +0300

    User model and repository added

---
+++ Web/Lab4/src/main/java/org/acrighthere/lab4/backend/model/User.java
@@ -0,0 +1,18 @@
+package org.acrighthere.lab4.backend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true,nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+}
